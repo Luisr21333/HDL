8a7db94b0af360131d3ca07a00816ab44715640f
Luisr21333 <91759964+Luisr21333@users.noreply.github.com>
2023-03-16 10:38:18 -0700

    VHDL Johnson Counter example

---
+++ RTL/VHDL/Johnson Counter/Johnson Counter.srcs/sim_1/new/Johnson_Counter_tb.vhd
@@ -0,0 +1,51 @@
+library ieee;
+use ieee.std_logic_1164.all;
+  
+-- parameterized Johnson Counter  
+entity Johnson_Counter_tb is
+end Johnson_Counter_tb;
+  
+architecture behavioral of Johnson_Counter_tb is
+constant CLOCK_period : time := 20 ns;
+constant WIDTH : integer := 4;
+signal clk : std_logic := '0';
+signal rst : std_logic := '0';
+signal Q : std_logic_vector(3 downto 0);  
+    component Johnson_counter
+     generic (
+      WIDTH : integer := 4
+    );
+    port(
+         clk : in  std_logic;
+         rst : in  std_logic;
+         Q : out  std_logic_vector(WIDTH-1 downto 0)
+        );
+    end component;
+    
+begin  
+   uut: Johnson_counter port map (
+          clk => clk,
+          rst => rst,
+          Q => Q
+        );
+ 
+   clock_process: process
+   begin
+        clk <= '0';
+        wait for CLOCK_period/2;
+        clk <= '1';
+        wait for CLOCK_period/2;
+   end process;
+  
+   stim_process: process
+   begin       
+      wait for 100 ns; 
+      rst <= '1';   
+      wait for 100 ns; 
+      rst <= '0';
+      wait for CLOCK_period*15;     
+    
+      --end simulation
+      assert false report "Simulation Finished" severity failure;
+   end process;
+end architecture;
---
+++ RTL/VHDL/Johnson Counter/Johnson Counter.srcs/sources_1/new/Johnson_Counter.vhd
@@ -0,0 +1,28 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+-- parameterized Johnson Counter  
+entity Johnson_Counter is
+    generic (
+        WIDTH : integer := 4
+    );
+    port ( clk : in  STD_LOGIC;
+           rst : in  STD_LOGIC;
+           Q : out  STD_LOGIC_VECTOR (WIDTH-1 downto 0));
+end Johnson_Counter;
+ 
+architecture behavioral of Johnson_Counter is
+signal q_tmp: std_logic_vector(WIDTH-1 downto 0):= "0000";
+begin
+    process(clk,rst)
+    begin
+    --asynchronous active high reset
+    if rst = '1' then
+        q_tmp <= "1001"; 
+        elsif Rising_edge(clk) then
+            --shift left
+            q_tmp <= q_tmp(WIDTH-2 downto 0) & not q_tmp(WIDTH-1);  
+    end if;
+    end process;
+Q <= q_tmp;
+end behavioral;
