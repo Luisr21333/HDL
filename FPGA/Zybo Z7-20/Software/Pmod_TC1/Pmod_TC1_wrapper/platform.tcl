a01eef3b307b717a00e1f409814501ef66865379
Luisr21333 <91759964+Luisr21333@users.noreply.github.com>
2023-04-03 18:50:06 -0700

    Pmod TC1 SW code

---
+++ FPGA/Zybo Z7-20/Software/Pmod_TC1/Pmod_TC1_wrapper/platform.tcl
@@ -0,0 +1,23 @@
+# 
+# Usage: To re-create this platform project launch xsct with below options.
+# xsct C:\Users\luisr\Desktop\FPGA\Software\Zybo_Z7-20\Pmod_TC1\Pmod_TC1_wrapper\platform.tcl
+# 
+# OR launch xsct and run below command.
+# source C:\Users\luisr\Desktop\FPGA\Software\Zybo_Z7-20\Pmod_TC1\Pmod_TC1_wrapper\platform.tcl
+# 
+# To create the platform in a different location, modify the -out option of "platform create" command.
+# -out option specifies the output directory of the platform project.
+
+platform create -name {Pmod_TC1_wrapper}\
+-hw {C:\Users\luisr\Desktop\FPGA\Hardware\Zybo Z7-20\Pmod_TC1\Pmod_TC1_wrapper.xsa}\
+-out {C:/Users/luisr/Desktop/FPGA/Software/Zybo_Z7-20/Pmod_TC1}
+
+platform write
+domain create -name {standalone_ps7_cortexa9_0} -display-name {standalone_ps7_cortexa9_0} -os {standalone} -proc {ps7_cortexa9_0} -runtime {cpp} -arch {32-bit} -support-app {hello_world}
+platform generate -domains 
+platform active {Pmod_TC1_wrapper}
+domain active {zynq_fsbl}
+domain active {standalone_ps7_cortexa9_0}
+platform generate -quick
+platform generate
+platform generate
