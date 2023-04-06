da3ba6eba8960dee3f9a4977f01fcbdff677b25b
Luisr21333 <91759964+Luisr21333@users.noreply.github.com>
2023-04-06 10:46:47 -0700

    VHDL Synchronizer example

--- RTL/VHDL/Synchronizer/Synchronizer.srcs/sources_1/new/Synchronizer.vhd
+++ RTL/VHDL/Synchronizer/Synchronizer.srcs/sources_1/new/Synchronizer.vhd
@@ -15,7 +15,7 @@ signal delayed_async : std_logic := '0';
 begin
 
     -- Delay the asynchronous input signal for one clock cycle
-    process (clk)
+    Delay_Process: process (clk)
     begin
         if rising_edge(clk) then
             delayed_async <= async_in; -- 1st clock cycle
@@ -24,7 +24,7 @@ begin
 
     -- Synchronize the delayed asynchronous signal to the clock domain of clk
     -- This ensures that the input signal has enough time to stabilize before it is sampled by the synchronizer process
-    process (clk)
+    Synchronize_Process: process (clk)
     begin
         if rising_edge(clk) then
             sync_out <= delayed_async; -- 2nd clock cycle
